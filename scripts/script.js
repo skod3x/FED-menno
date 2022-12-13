a09e10b9d6f6830441f7ba73de32921bc0f77281
Menno Leurs <mennojl@hotmail.com>
2022-12-13 16:41:15 +0100

    yolo

--- scripts/script.js
+++ scripts/script.js
@@ -2,16 +2,14 @@
 /* menu openen de MENU button */
 /******************************/
 
-/* JOUW CODE HIER - stap 4 */
-
-// stap 1: zoek de menu-button op en sla die op in een variabele
+// menu-button opslaan in variable
 var openButton = document.querySelector("header > button");
 
-// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+// menu button latel luisteren naar een click en functie laten uitvoeren
 openButton.addEventListener("click", openMenu);
 
-// stap 3: voeg in de functie een class toe aan de nav
-function openMenu() {  
+// function voegt class toe aan nav
+function openMenu() {
   // zoek de nav op
   var deNav = document.querySelector("nav");
   // voeg class toe aan nav
@@ -19,27 +17,21 @@ function openMenu() {
 }
 
 
-
 /************************************/
 /* menu sluiten met de sluit button */
 /************************************/
 
-/* JOUW CODE HIER - stap 5 */
-
-// stap 1 - zoek sluiten button op
+// sluit button opslaan in variable
 var sluitButton = document.querySelector("header nav button");
 
-// stap 2 - laat die button luisteren naar kliks
+// sluit button laten luisteren naar een click en functie laten uitvoeren
 sluitButton.addEventListener("click", sluitMenu);
 
-// stap 3 - in de functie verwijder de class van de nav
+//functie voor verwijderen class van de nav
 function sluitMenu() {
   var deNav = document.querySelector("nav");
   deNav.classList.remove("toonMenu");
 }
-
-
-
 
 /**********************************/
 /* menu sluiten met escape */
